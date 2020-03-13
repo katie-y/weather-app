247319241481cdb083838c5f1a084cc5a49e7762
katie-y <katie_york@hotmail.co.uk>
2020-03-13 15:45:40 +0000

    created a weather app

---
+++ README.md
@@ -0,0 +1,2 @@
+# express-handlebars-base
+node
---
+++ index.js
@@ -0,0 +1,27 @@
+const hbs = require('express-handlebars');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+require('dotenv').config();
+
+const getWeather = require('./lib/getWeather');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('.hbs', hbs({
+    defaultLayout: 'layout',
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+app.get('/', async (req, res) => {
+    let data = await getWeather();
+    console.log(data);
+    res.render('index', {data: 'Hello from Express'});
+});
+
+app.listen(3000, () => {
+    console.log('server listening on port 3000');
+});
